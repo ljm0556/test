37cd7f9e4b9911c7135c031799a840928722bb27
justicechannn <cks4315@naver.com>
2022-09-01 17:58:25 +0900

    워ㅗㄹ

---
+++ SQL월드컵정리.sql
@@ -0,0 +1,147 @@
+걸그룹
+
+insert into URL values('예지','https://thumbs.gfycat.com/EsteemedShowyFantail-size_restricted.gif',0,1);
+insert into URL values('아이린 ','https://thumbs.gfycat.com/EsteemedShowyFantail-size_restricted.gif',0,2);
+insert into URL values('수지','https://www.dogdrip.net/dvs/b/i/17/12/15/78/515/122/148/148c3644e9b7bcbbeb3c2cc0c61836e8.gif',0,3);
+insert into URL values('지호','http://cdn.dealbada.com/data/editor/1909/7fe08c3fcdde99fd31f6fbd860eb3583_1567773514_9195.gif',0,4);
+insert into URL values('류진','https://thumbs.gfycat.com/SilverThunderousInvisiblerail-size_restricted.gif',0,5);
+insert into URL values('보나','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFR3VUm9WUR9b195UxZlRDe42hbEtTTzmjcA&usqp=CAU',0,6);
+insert into URL values('아이유','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUByogF4SY7JeJAajyZdxyjqCqhTqIZGPLFQ&usqp=CAU',0,7);
+insert into URL values('혜리','https://t1.daumcdn.net/cafeattach/mEr9/91652e3d982793e882e4ff445265b81703f5bfe8',0,8);
+insert into URL values('소연','https://t1.daumcdn.net/cafeattach/mEr9/6a2627857298ac0584d0daf80f7407343ae22209',0,9);
+insert into URL values('미나','http://img1.daumcdn.net/thumb/R1024x0/?fname=https://t1.daumcdn.net/cafeattach/Uzlo/041e411b2f1f38cc92b5cb7341b8b7be449e49b6',0,10);
+insert into URL values('나연 ','https://i.pinimg.com/originals/fb/07/2d/fb072da73a764e7a65918ece2fdaafd4.gif',0,11);
+insert into URL values('도아','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSogrOZZO3D1RCRGg6IXuyNJtLdEKbYzCq8yA&usqp=CAU',0,12);
+insert into URL values('피나틱스나연','https://thumbs.gfycat.com/DigitalThankfulChinesecrocodilelizard-mobile.mp4',0,13);
+insert into URL values('체리블렛해윤','https://dimg.donga.com/ugc/CDB/29STREET/Article/5e/70/7c/de/5e707cde071ed273825d.gif',0,14);
+insert into URL values('슬기','https://mblogthumb-phinf.pstatic.net/MjAxODA2MTBfMiAg/MDAxNTI4NTk5NzI1Nzg4.k14LWYG1scRFtKIBv2I1VSdvVM63J3e07c_e4Y6J4r4g.pj7ikE6pTKzoOii9O34tXGl3fvtjMfG8MaXreBud0Dkg.GIF.kmh33080/GIFvu8wTz04Ejflke3tXi.gif?type=w800',0,1);
+insert into URL values('선미','https://coinpan.com/files/attach/images/313/273/819/164/f1d433a689607da7ec1ae339ed724e17.gif',0,15);
+insert into URL values('다이아','http://www.stardailynews.co.kr/news/photo/201611/122990_148576_3058.gif',0,16);
+insert into URL values('위클리지한','https://c.tenor.com/I8blM7YVs7QAAAAd/%EC%B0%8C%ED%95%98%EB%8B%88-%EC%A7%80%ED%95%9C%EC%9D%B4.gif',0,17);
+insert into URL values('르세라핌허윤진','https://img.hankyung.com/photo/202205/03.29834578.1.jpg',0,18);
+insert into URL values('위키미키루아','https://t1.daumcdn.net/cfile/tistory/993E25485C324BF608',0,19);
+insert into URL values('라붐진예','https://t1.daumcdn.net/cfile/tistory/993621335B69875B0A',0,20);
+insert into URL values('에이핑크손나은 ','https://blog.kakaocdn.net/dn/s3GHc/btqFQKEnLYU/JExpmLXUW3lt4zHiW9VYLk/img.gif',0,21);
+insert into URL values('미연','https://thumbs.gfycat.com/BoilingThisEyra-size_restricted.gif',0,22);
+insert into URL values('유주','https://dimg.donga.com/wps/NEWS/IMAGE/2022/01/18/111304295.2.jpg',0,23);
+insert into URL values('웬디','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY5c-sP2rHA0WsxrfiIeDQ07JdPaV6u5n5Ig&usqp=CAU',0,24);
+insert into URL values('조이','https://c.tenor.com/lwq89ze5QjAAAAAC/%EC%A1%B0%EC%9D%B4-%EB%A0%88%EB%93%9C%EB%B2%A8%EB%B2%B3.gif',0,25);
+insert into URL values('세러데이유키','https://img.hankyung.com/photo/201807/03.17327417.1.jpg',0,26);
+insert into URL values('제니','https://i.pinimg.com/originals/b8/3c/08/b83c08d1733195baac9838b45dbfdf16.gif',0,27);
+insert into URL values('모모랜드혜빈','https://t1.daumcdn.net/cfile/tistory/99820F4F5E1B0E9824',0,28);
+insert into URL values('아이브이서','https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2021/12/01/ca1e8a88-ea22-4ba5-af72-6e65fdc8ac22.gif',0,29);
+insert into URL values('드림캐쳐가현','https://t1.daumcdn.net/cfile/tistory/9996B23359D6329324',0,30);
+insert into URL values('원터','https://fl.gr/data/file/aespa/2944011544_eTq7Xkmj_513f9eff52e813a03c54414223ccce3c21ac7b36.gif',0,31);
+insert into URL values('카리나','https://img1.daumcdn.net/thumb/R1024x0/?fname=https://t1.daumcdn.net/cafeattach/mEr9/a5887d2a7923f3de8c68b1d9cfcb719e42c17d2a',0,32);
+
+insert into URL('이름미정','',0,3);
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+음식
+insert into URL values('감자탕','https://cdn.wadiz.kr/ft/images/green001/2020/0828/20200828122515851_50.gif',0,2);
+insert into URL values('치킨','https://photo.coolenjoy.co.kr/data/editor/2205/e693bfce64f105bb79c6d74dcfe79853eff94d16.gif',0,2);
+insert into URL values('피자','https://www.cjthemarket.com/cjom/design/detail/prd_detail_gourmet_pizza0103.gif',0,2);
+insert into URL values('족발','https://cdn.wadiz.kr/ft/images/green001/2021/0503/20210503033911381_41.gif',0,2);
+insert into URL values('순대국','https://blog.kakaocdn.net/dn/DPOKr/btqVddKI408/pFwQDzJHIEH5DF5kuAaPGk/img.gif',0,2);
+insert into URL values('삼겹살','https://cdn.wadiz.kr/ft/images/green001/2022/0613/20220613215346716_25.gif',0,2);
+insert into URL values('소갈비','https://2.bp.blogspot.com/-Qz6VnmJIYGA/W3aM_T3eO4I/AAAAAAAEg3Q/g-x-bIvyTJoRVEjPq1axysv2yo-Ny--cgCLcBGAs/s1600/7.gif',0,2);
+insert into URL values('짜장면','https://t1.daumcdn.net/liveboard/Cookat/96d4e98bd802451faefe75daae6fdf2f.gif',0,2);
+insert into URL values('짬뽕','http://file3.instiz.net/data/file3/2022/02/24/d/8/0/d8045b68ad6589b13a1bad9ec10eb8d6.gif',0,2);
+insert into URL values('보쌈','https://blog.kakaocdn.net/dn/bDW6af/btrrQzPt9in/O7icAE5Otx7qtFr9NS4EuK/img.gif',0,2);
+insert into URL values('닭갈비','http://file3.instiz.net/data/file3/2021/07/14/6/7/3/673bf41315011aedc42bbab294b5cf96.gif',0,2);
+insert into URL values('찜닭','http://file3.instiz.net/data/file3/2018/03/18/2/a/2/2a2428cf139c91cf0d67d275c1fa10b9.gif',0,2);
+insert into URL values('햄버거','https://www.expatchoice.asia/sites/default/files/inline-images/Webp.net-gifmaker%20-%202021-05-23T132428.870_0.gif',0,2);
+insert into URL values('샌드위치','https://ca-times.brightspotcdn.com/dims4/default/c8173b3/2147483647/strip/true/crop/1080x720+0+0/resize/840x560!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Feb%2Ffc%2Fd444a77444ec8f25572bc947d3b3%2Fla-fo-sublist.gif',0,2);
+insert into URL values('볶음밥','https://img.jjang0u.com/data4/docs/160/202008/06/25/b808cbf74b5b58f51fe437288cfc2c24_596900.gif',0,2);
+insert into URL values('탕수육','https://gif.fmkorea.com/files/attach/new/20170623/486616/412183146/690853210/ba68d24f4f80bf31b0dd0c59cf94ed56.gif',0,2);
+insert into URL values('리조또','https://mblogthumb-phinf.pstatic.net/MjAyMTEwMDNfOTEg/MDAxNjMzMjM4NzU1MTY5.0pqoW8YtXrdUKbCKRJ_iYCYHSNGLhrOmSRrkn6kDseQg.sa4rLgpJ13fBvPXiGLNQc4thIMMzi45Dy9lm04q-5DYg.GIF.wkdtjdrb132/SmartSelect%EF%BC%BF20211003%EF%BC%8D142544%EF%BC%BFGallery.gif?type=w800',0,2);
+insert into URL values('라면','https://mblogthumb-phinf.pstatic.net/MjAxODA0MjVfMzYg/MDAxNTI0NjIxMjY0ODEz.Tf3URyGTbJap4bbONkMGQqcxYSdsv-xBaDXZob2qymAg.ga7LA6aER6punvi9YFDd0cB0psq-SUqzuX55VOla6RQg.GIF.kiwoomct/%EB%9D%BC%EB%A9%B4.gif?type=w2',0,2);
+insert into URL values('제육볶음','https://thumbs.gfycat.com/LeftHeartfeltAlleycat-size_restricted.gif',0,2);
+insert into URL values('갈비탕','https://t1.daumcdn.net/cfile/tistory/99BCA54E5E992B5827',0,2);
+insert into URL values('물냉면','http://file3.instiz.net/data/file3/2018/10/14/d/b/1/db12e8daa461179d8509eebd1425fc21.gif',0,2);
+insert into URL values('막국수','https://mblogthumb-phinf.pstatic.net/MjAxOTA3MDJfMjcz/MDAxNTYyMDUwOTYwOTg5.i-OsI1i7C0cQ1x2VUHaUfq72mfODMcXt2Ny5nIWQuiQg.uWhi1oze1eRzhJ674nf3AZzCkRwBk26d-9AWXhI4-fkg.GIF.vgomugomv/%EB%A7%89%EA%B5%AD%EC%88%982.gif?type=w800',0,2);
+insert into URL values('전','https://thingool123.godohosting.com/data/goods/editor/220223/1_183702.gif',0,2);
+insert into URL values('카레','https://post-phinf.pstatic.net/MjAxOTA2MTRfMTE3/MDAxNTYwNDc3ODg0MDMz.dacnOu3kKa_zhsUWbHeWsofNpFMkNsxU4P3yNd2-_DYg.oRHveZwKAubCCshgn2FNenWcJ52u5wpfx-TqXxv5cZEg.GIF/%EC%9B%80%EC%A7%A4_4.gif?type=w1200',0,2);
+insert into URL values('곱창','https://mblogthumb-phinf.pstatic.net/MjAxOTEwMDJfMTQ2/MDAxNTY5OTQyODQ3MDIy.j_SW9Zj9Yy7lv7OoPxD_bBB0kdQ2qxMU6cyLt38uJQcg.N4W7rrVxeQWCo-HwLVzcjgX6YDx2Q9McaYM3ihCQh70g.GIF.uenojuri7/IMB_H5Bgkc.gif?type=w800',0,2);
+insert into URL values('닭도리탕','http://t1.daumcdn.net/thumb/R1024x0/?fname=http://m1.daumcdn.net/cfile209/image/99E9EA3B5B7E31083582C9',0,2);
+insert into URL values('물회','https://i2.tcafe2a.com/220605/defa218bee9b3144fdae312e11810bef_1654411956_8151.gif',0,2);
+insert into URL values('초밥','https://mblogthumb-phinf.pstatic.net/MjAyMDA3MDdfNjAg/MDAxNTk0MTI2MjcyNTk5.d0bXXpupDycV61AFIsAVT1rZdAIxf_4bRdowg1Wz8dcg.PcOiZa8ezlV0FdOuG7yWYshMnMaXuMhGeUT4BPI7SC8g.GIF.dl1293/IMB_0D4vHT.gif?type=w800',0,2);
+insert into URL values('뼈해장국','https://blog.kakaocdn.net/dn/1d4YY/btrsQqLeJvk/CnO1AbdRZ57kIZk40tWolK/img.gif',0,2);
+insert into URL values('양갈비','https://blog.kakaocdn.net/dn/cxisaS/btqSIYo6vxM/ErupdmyQFt2hjidACUkOE1/img.gif',0,2);
+insert into URL values('청국장','https://mblogthumb-phinf.pstatic.net/MjAxODAzMjVfMjM4/MDAxNTIxOTg1NDA1MDY5.VhaeIntbaKLM8l-aJUYTG3QN9PcoAaqCqfZHelmCN1Ig.c1xT_ZOnKELZXuXmLKYWsTKXVY674VpvNymas6F7wAwg.GIF.wannajun00/%EB%8F%BC%EC%A7%80%EA%B3%A0%EA%B8%B0%EB%90%9C%EC%9E%A5%EC%B0%8C%EA%B0%9C_%EB%A7%9B%EC%9E%88%EA%B2%8C%EB%81%93%EC%9D%B4%EB%8A%94%EB%B2%95_%EB%B0%B1%EC%A2%85%EC%9B%90%ED%99%A9%EA%B8%88%EB%A0%88%EC%8B%9C%ED%94%BC17.gif?type=w800',0,2);
+insert into URL values('김치찌개','https://t1.daumcdn.net/cfile/tistory/190854494DFC08FF2E',0,2);
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+insert into THEME values(3,'actor_man',0);
+
+남자배우
+insert into URL values('주원','https://i2.ruliweb.com/ori/16/08/16/1569274603346c91a.gif',0,3);
+insert into URL values('김래원','https://t1.daumcdn.net/cfile/tistory/2175EE34579ED51823',0,3);
+insert into URL values('하정우','https://4.bp.blogspot.com/-qIaKbqBdvFk/VeGI6KA2vfI/AAAAAAAABPQ/x7AuOz4bGPw/s1600/%25EA%25B9%2580.gif',0,3);
+insert into URL values('강동원','https://image.ajunews.com/content/image/2019/06/15/20190615013828623968.gif',0,3);
+insert into URL values('마동석','https://img.extmovie.com/files/attach/images/135/308/382/051/cdde896ee7e1a9e9bf90b58d20d9131c.gif',0,3);
+insert into URL values('황정민','https://i.imgur.com/RqEbUEt.gif',0,3);
+insert into URL values('송중기','https://blog.kakaocdn.net/dn/U9rKf/btqXYqutRKv/cqWfC1cXtd0AXWtw42l8KK/img.gif',0,3);
+insert into URL values('이광수','https://t1.daumcdn.net/liveboard/sbsnoriter/a1623c1b46bf49958ada610025de9d63.gif',0,3);
+insert into URL values('최우식','https://t1.daumcdn.net/cfile/tistory/994A21345F104C192A',0,3);
+insert into URL values('박서준','https://blog.kakaocdn.net/dn/PFt8F/btqB6jCD6S7/b0bFgkr4PsEH4J7Q06p9Sk/img.gif',0,3);
+insert into URL values('임창정','https://media.tenor.com/images/b2b9665a0ecd8321ead7e188d6d04b66/tenor.gif',0,3);
+insert into URL values('이정재','https://www.waiternara.kr/gb4/data/file/talk002/2950630935_HfoB39cW_B72C4581-AC83-41AD-BE0E-B431C2157F01.gif',0,3);
+insert into URL values('강하늘','https://i.pinimg.com/originals/72/30/6c/72306c4fa88525470112391498e4d29e.gif',0,3);
+insert into URL values('서인국','https://blog.kakaocdn.net/dn/dzFrxE/btq4UreiPL3/hakATwuMIXOPQPFUkQK811/img.gif',0,3);
+insert into URL values('이병헌','https://coinpan.com/files/attach/images/198/646/118/214/9c2bdbcb2ccebe98658449e855110951.gif',0,3);
+insert into URL values('옥택연','https://blog.kakaocdn.net/dn/QSRjK/btqXXVnY0iw/rKkyBkBoVa8faGvgCKZiPK/img.gif',0,3);
+insert into URL values('남주혁','https://blog.kakaocdn.net/dn/2znBr/btruqGsNlYF/DA2ctliYSVP0MaApZAvbr0/img.jpg',0,3);
+insert into URL values('유승호','https://img.insight.co.kr/static/2019/01/14/700/1as7621895186v7osi9m.jpg',0,3);
+insert into URL values('이민호','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXL7mbeupimZTBEUfJ4bXIrDMaJQiVQHMl3w&usqp=CAU',0,3);
+insert into URL values('김선호','https://t1.daumcdn.net/liveboard/SMG/919335f588174c9bb5bc6fb471e26c40.JPG',0,3);
+insert into URL values('장기용','https://i.pinimg.com/originals/cd/c8/a2/cdc8a20e1b405963d9bfdd31d814d250.jpg',0,3);
+insert into URL values('우도환','https://img.insight.co.kr/static/2019/11/17/700/11dl9o66pua09ar1s4ve.jpg',0,3);
+insert into URL values('박보검','http://img.etoday.co.kr/pto_db/2021/03/20210316105605_1594282_700_385.jpg',0,3);
+insert into URL values('이재욱','https://i.pinimg.com/736x/15/d1/c8/15d1c8ea0fd456247f2d619330d19f14.jpg',0,3);
+insert into URL values('원빈','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZF5VDKhupOIYXd_lndDwlEUe09RkeRc76uQ&usqp=CAU',0,3);
+insert into URL values('윤종현','https://images.chosun.com/resizer/Y4uvQ-r05c3xMFhRliLelSXdq9E=/500x330/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/4SXTUGL3DSPCLDCSPOZZVNKO7A.jpg',0,3);
+insert into URL values('위하준','https://img.seoul.co.kr/img/upload/2021/11/11/SSI_20211111225524_O2.jpg',0,3);
+insert into URL values('유아인','http://imgur.com/weS9qy6.jpg',0,3);
+insert into URL values('이도현','https://i.pinimg.com/originals/e4/08/d8/e408d8568826f31b881b9700f6ed60aa.gif',0,3);
+insert into URL values('김지훈','https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/07/01/b1657fa4-d8be-416f-8896-2c1ed4c26fe2.jpg',0,3);
+insert into URL values('이승기','https://img6.yna.co.kr/etc/inner/KR/2013/07/17/AKR20130717184000005_03_i_P4.jpg',0,3);
+insert into URL values('이정재','https://t1.daumcdn.net/cfile/tistory/245A813557E6F7E019',0,3);
+insert into URL values('전우성','https://fimg4.pann.com/new/download.jsp?FileID=49605438',0,3);
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+insert into URL values('캥거루','https://t1.daumcdn.net/cfile/tistory/9934E33359A2A7060C',0,4);
+insert into URL values('나무늘보','https://t1.daumcdn.net/cfile/tistory/99F6E83359A2A70A2D',0,4);
+insert into URL values('코알라','https://t1.daumcdn.net/cfile/tistory/9996AF3359A2A70E34',0,4);
+insert into URL values('호랑이','https://t1.daumcdn.net/cfile/tistory/994E9A3359A2A71005',0,4);
+insert into URL values('고슴도치','https://t1.daumcdn.net/cfile/tistory/99DBDF3359A2A7140C',0,4);
+insert into URL values('뱀','https://t1.daumcdn.net/cfile/tistory/99E2D03359A2A71816',0,4);
+insert into URL values('북극곰','https://t1.daumcdn.net/cfile/tistory/99A8EC3359A2A71819',0,4);
+insert into URL values('오리','https://t1.daumcdn.net/cfile/tistory/9921EC3359A2A71B13',0,4);
+insert into URL values('표범','https://t1.daumcdn.net/cfile/tistory/995A7A3359A2A71C2E',0,4);
+insert into URL values('거북이','https://t1.daumcdn.net/cfile/tistory/99E4583359A2A74D25',0,4);
+insert into URL values('사막여우','https://t1.daumcdn.net/cafeattach/aVeZ/898a6303781086872bde4cff2742b1644385403b',0,4);
+insert into URL values('판다','https://mblogthumb-phinf.pstatic.net/20131104_287/cevinfocus_1383569222949lR4hu_GIF/%C6%D2%B4%F54.gif?type=w2',0,4);
+insert into URL values('레서판다','https://i.imgur.com/dPalhzy.gif',0,4);
+insert into URL values('사슴','https://i.imgur.com/gGBcjSg.gif',0,4);
+insert into URL values('강아지','http://file.instiz.net/data/file/20140711/6/e/2/6e23b052576ca5be9f5574306b75732f.gif',0,4);
+insert into URL values('고양이','https://t1.daumcdn.net/cafeattach/mEr9/c7e3e8dca9e00dc3cb57e805fe5a48131e708e7a',0,4);
+insert into URL values('부엉이','https://i.pinimg.com/originals/4f/0f/1d/4f0f1d0e6767ba1427f9caf0740ace9a.gif',0,4);
+insert into URL values('코끼리','https://t1.daumcdn.net/cfile/tistory/9906DB3359A2A7212C',0,4);
+insert into URL values('족제비','https://social-phinf.pstatic.net/20210621_78/1624270158622L5Rkg_GIF/cbaca91cd74c4e44b9a0e78946f5fa201923a3c2_re_1623859411524.gif',0,4);
+insert into URL values('해달','https://mblogthumb-phinf.pstatic.net/MjAyMDAzMTFfMjA0/MDAxNTgzOTAzODc1NzI1.NPgqNS2Q9dA10f1hzN30XF10aHjMjXV9_JYheZvhgNYg.WoW0z6DS7GagUlz3GnLkK-GULLDr0lGCFcs33A2IzCog.GIF.shinjicho/IMG_7618.GIF?type=w800',0,4);
+insert into URL values('하프물범','http://file3.instiz.net/data/file3/2019/10/06/4/0/7/4076b28955b9d194b89c8820adc82adf.gif',0,4);
+insert into URL values('토코투칸','http://upload2.inven.co.kr/upload/2019/10/26/bbs/i14613420393.gif',0,4);
+insert into URL values('코뿔소','http://file3.instiz.net/data/file3/2018/03/27/1/5/7/1572df84cfc40e9045cd32dc659d0c50.gif',0,4);
+insert into URL values('올빼미','https://t1.daumcdn.net/cfile/blog/19143C434FB8F1931B',0,4);
+insert into URL values('악어','https://t1.daumcdn.net/cfile/tistory/99ED553359A2A7092D',0,4);
+insert into URL values('붉은여우','https://i.pinimg.com/originals/f5/97/13/f5971317c838e5f067af97a71b39e3a5.gif',0,4);
+insert into URL values('알','https://gaegoods.com/web/upload/NNEditor/20190315/LANCO_2018_all03_EB9DBCECA780_EC9791EC8AA4EB9DBCECA780EB9DBCEB8B88_shop1_153618.gif',0,4);
+insert into URL values('양','https://issuya.com/data/editor/2105/16208917553951.gif',0,4);
+insert into URL values('펭귄','https://thumbs.gfycat.com/ValidWealthyDuiker-size_restricted.gif',0,4);
+insert into URL values('원숭이','https://t1.daumcdn.net/cfile/tistory/999B923359A2A70833',0,4);
+insert into URL values('곰','https://t1.daumcdn.net/cfile/tistory/99A1743359A2A7112B',0,4);
+insert into URL values('늑대','https://i.pinimg.com/originals/10/5a/94/105a94d192394b7d1abe5f099319146e.gif',0,4);
+
+
+
